b298ebf94a65add3a75a086e0219242d852c4906
Davide D'Antonio <davide.dantonio@webeetle.com>
2019-12-16 12:21:45 +0100

    editor config

--- src/components/Forms/CheckboxWrapper.js
+++ src/components/Forms/CheckboxWrapper.js
@@ -5,66 +5,66 @@ import { makeStyles } from '@material-ui/core/styles';
 import PropTypes from "prop-types";
 
 const useStyles = makeStyles({
-    root: {
-        color: props => props.hasError ? 'red' : 'initial',
-    },
-    label: {
-        color: props => props.hasError ? 'red' : 'initial'
-    },
-    labelPlacementStart: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        marginLeft: 0
-    }
+  root: {
+    color: props => props.hasError ? 'red' : 'initial',
+  },
+  label: {
+    color: props => props.hasError ? 'red' : 'initial'
+  },
+  labelPlacementStart: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    marginLeft: 0
+  }
 });
 
 const CheckboxWrapper = (props) => {
-    const {
-        input: {name, onChange, value, checked},
-        meta,
-        label,
-        disabled,
-        labelPlacement,
-        color = 'primary',
-    } = props;
+  const {
+    input: { name, onChange, value, checked },
+    meta,
+    label,
+    disabled,
+    labelPlacement,
+    color = 'primary',
+  } = props;
 
-    const classes = useStyles({
-        hasError: meta.error && meta.touched,
-        labelPlacement
-    });
+  const classes = useStyles({
+    hasError: meta.error && meta.touched,
+    labelPlacement
+  });
 
-    return (
-        <FormControlLabel
-            name={name}
-            onChange={(...params) => {
-                if(onChange){
-                    onChange(...params)
-                }
-                if(props.onChange){
-                    props.onChange(...params)
-                }
-            }}
-            value={value}
-            checked={checked}
-            disabled={disabled}
-            control={
-                <Checkbox color={color} classes={classes}/>
-            }
-            label={label}
-            labelPlacement={labelPlacement}
-            classes={classes}
-        />
-    );
+  return (
+    <FormControlLabel
+      name={name}
+      onChange={(...params) => {
+        if (onChange) {
+          onChange(...params)
+        }
+        if (props.onChange) {
+          props.onChange(...params)
+        }
+      }}
+      value={value}
+      checked={checked}
+      disabled={disabled}
+      control={
+        <Checkbox color={color} classes={classes} />
+      }
+      label={label}
+      labelPlacement={labelPlacement}
+      classes={classes}
+    />
+  );
 };
 
 CheckboxWrapper.propTypes = {
-    input: PropTypes.object,
-    meta: PropTypes.object,
-    fullWidth: PropTypes.bool,
-    disabled: PropTypes.bool,
-    labelPlacement: PropTypes.string,
-    color: PropTypes.string,
-    onChange: PropTypes.func,
+  input: PropTypes.object,
+  meta: PropTypes.object,
+  fullWidth: PropTypes.bool,
+  disabled: PropTypes.bool,
+  labelPlacement: PropTypes.string,
+  color: PropTypes.string,
+  onChange: PropTypes.func,
 }
 
 export default CheckboxWrapper;
