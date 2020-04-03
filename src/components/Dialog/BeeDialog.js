b01627ad4e2b6b8dd46bc59ae8ce6e23b246288d
davidedantonio <davide.dantonio@webeetle.com>
2020-04-03 14:46:46 +0200

    fix

--- src/components/Dialog/BeeDialog.js
+++ src/components/Dialog/BeeDialog.js
@@ -78,7 +78,7 @@ BeeDialog.propTypes = {
   open: PropTypes.bool.isRequired,
   title: PropTypes.any,
   toolbarBtns: PropTypes.node,
-  maxWidth: PropTypes.number,
+  maxWidth: PropTypes.string,
   children: PropTypes.node,
   styleContent: PropTypes.object,
   onClose: PropTypes.func,
